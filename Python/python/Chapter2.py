bd55b505b92946b882eb9a77075c2a29ce42a102
Amy1481 <amishamohanty2025@gmail.com>
2026-03-09 21:47:30 +0530

    message

---
+++ Python/python/Chapter2.py
@@ -0,0 +1,32 @@
+# Variables and Datatype
+a = 1 #a is int type
+b = 2
+print(a+b)
+
+c=3.44 #c is floating point type
+d="harry" #d is string
+e=True #boolean type
+f=None #none type
+
+# Operators
+# Arithmetic +,-,*,/
+# Assignment =,+=,-=
+# Comparison ==,>,>=,!=
+# Logical and,or,not
+
+# Type
+c = 32.2
+t =type(c) #class <float>
+print(t)
+
+d = "32.2"
+e = float(d) #a but the type should be float
+t =type(e) 
+print(t)
+
+# Input
+f=input("Enter number 1")
+g=input("Enter number 2")
+print("Number f is: ",f)
+print("Number g is: ",g)
+print("Sum is: ", f+g) #Adds the number as in 1 and 2 gives 12
