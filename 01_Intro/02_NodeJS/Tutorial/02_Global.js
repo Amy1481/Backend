c0498e28d84833e2d921dfd5223001dbf07fe528
Amy1481 <amishamohanty2025@gmail.com>
2026-03-13 20:01:44 +0530

    Add OOPs assignments structure

--- 01_Intro/02_NodeJS/Tutorial/02_Global.js
+++ 01_Intro/02_NodeJS/Tutorial/02_Global.js
@@ -1,4 +1,13 @@
-// Globals
+// Chapter5:Globals
+// Globals - No Window!!!
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules(CommonJS)
+// module - info about current module(file)
+// process - info about env where the program is being executed
+
+
+
 console.log(__dirname)
 setInterval(() => {
     console.log('hello world');
